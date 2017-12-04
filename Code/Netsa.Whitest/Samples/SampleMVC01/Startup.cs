8ad256bded5823b7410cbbb1a9809ba2c3b35135
Hadi <Ahmadi.hadi@outlook.com>
2017-12-04 12:24:16 +0330

    Sample Project Initialized #4

---
+++ Code/Netsa.Whitest/Samples/SampleMVC01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleMVC01.Startup))]
+namespace SampleMVC01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
