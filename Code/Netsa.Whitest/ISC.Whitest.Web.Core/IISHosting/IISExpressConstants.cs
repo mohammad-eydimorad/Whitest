d646df8ac21775505e1b89be969159a67c548bee
Hadi <Ahmadi.hadi@outlook.com>
2017-12-04 12:08:30 +0330

    (ConfigService + Tests ) & IISExpress initialized

---
+++ Code/Netsa.Whitest/ISC.Whitest.Web.Core/IISHosting/IISExpressConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISC.Whitest.Web.Core.IISHosting
+{
+    public static class IISExpressConstants
+    {
+        public const string IISExpressX86Path = @"C:\Program Files (x86)\IIS Express\iisexpress.exe";
+        public const string IISExpressPath = @"C:\Program Files\IIS Express\iisexpress.exe";
+    }
+}
---
+++ Code/Netsa.Whitest/ISC.Whitest.Web.Core/WebTestConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace ISC.Whitest.Web.Core
+{
+    public class WebTestConfiguration
+    {
+        public bool UseIISExpress { get; private set; }
+        public string IISExpressPath { get; private set; }
+
+        internal WebTestConfiguration(bool useIISExpress, string IISExpressPath)
+        {
+            this.UseIISExpress = useIISExpress;
+            this.IISExpressPath = IISExpressPath;
+        }
+    }
+}
