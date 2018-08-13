b7e57c7c268c7c9c212f19dd5e5063ce71a8eaa8
M_Eydimorad <M_Eydimorad@M-EYDIMORAD-10>
2018-08-13 13:46:56 +0430

    ScenarioContext AddOrUPdate method name

--- Code/Netsa.Whitest/ISC.Whitest.Web.Core/Steps/BaseStep.cs
+++ Code/Netsa.Whitest/ISC.Whitest.Web.Core/Steps/BaseStep.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using ISC.Whitest.Web.Core.Context;
@@ -16,6 +17,16 @@ namespace ISC.Whitest.Web.Core.Steps
         protected BaseStep(ScenarioContext currentScenarioContext)
         {
             CurrentScenarioContext = new SpecflowScenarioContextAdapter(currentScenarioContext);
+        }
+
+        public void CallGiven<T>(string methodName)
+        {
+            var type = typeof(T);
+            var method = type.GetMethods().First(a => a.Name.Equals(methodName));
+
+            var givenAttribute = method.GetCustomAttributes<GivenAttribute>().First();
+            var attributeText = givenAttribute.Regex;
+            Given(attributeText);
         }
     }
 }
