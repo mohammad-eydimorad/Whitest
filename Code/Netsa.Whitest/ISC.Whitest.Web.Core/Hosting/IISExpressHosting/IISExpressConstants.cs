d834e1a0de0461366bd0a5a6f42a16d678388174
pc1990 <single1990@gmail.com>
2018-10-01 17:19:02 +0330

    GetPathRoot

--- Code/Netsa.Whitest/ISC.Whitest.Web.Core/Hosting/IISExpressHosting/IISExpressConstants.cs
+++ Code/Netsa.Whitest/ISC.Whitest.Web.Core/Hosting/IISExpressHosting/IISExpressConstants.cs
@@ -1,8 +1,13 @@
-﻿namespace ISC.Whitest.Web.Core.Hosting.IISExpressHosting
+﻿using System;
+using System.IO;
+
+namespace ISC.Whitest.Web.Core.Hosting.IISExpressHosting
 {
+  
     public static class IISExpressConstants
     {
-        public const string IISExpressX86Path = @"C:\Program Files (x86)\IIS Express\iisexpress.exe";
+        public readonly static string path = Path.GetPathRoot(Environment.SystemDirectory);
+        public readonly static string IISExpressX86Path = path.Replace(@"\", "") +  @"\Program Files (x86)\IIS Express\iisexpress.exe";
         public const string IISExpressPath = @"C:\Program Files\IIS Express\iisexpress.exe";
     }
 }
